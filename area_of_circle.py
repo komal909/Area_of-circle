e9fc8314613d51d641d5007409fbfbc89a2e467b
komal909 <65548086+komal909@users.noreply.github.com>
2020-05-28 15:00:46 +0530

    created are_of_circle.py
    it shows the area of the circle on the screen.

---
+++ area_of_circle.py
@@ -0,0 +1,9 @@
+input :
+pi=22/7
+r=float(input("the radius of the circle: "))
+area=pi*(r**2)
+print("area of the circle with radius 1.1 is ",area )
+output:
+the radius of the circle:1.1
+area of the circle with radius 1.1 is  3.8028571428571434
+
